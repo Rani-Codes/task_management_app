dbba53e510967b9c1784f1f8c16498ba4300b79d
Rani-Codes <92886322+Rani-Codes@users.noreply.github.com>
2023-12-29 00:12:48 -0800

    updated routing locations on server.js

--- RestAPI/server.js
+++ RestAPI/server.js
@@ -1,6 +1,7 @@
 // Import dependencies
 const express = require("express");
 const userRoutes = require('./src/users/routes');
+const taskRoutes = require('./src/tasks/routes')
 
 // Create an express app
 const app = express();
@@ -9,12 +10,14 @@ const port = 3000;
 //Helps us get and use json from our endpoints
 app.use(express.json());
 
-//Routing 
+//Routing for default page, users, and tasks
 app.get("/", (req, res) => {
     res.send("Hello World!")
 })
 
-app.use("/api/v1/users", userRoutes);
+app.use("/api/users", userRoutes);
+
+app.use("/api/tasks", taskRoutes);
 
 // Start our server
 app.listen(port, () => console.log(`app listening on port ${port}`));
--- RestAPI/src/users/controller.js
+++ RestAPI/src/users/controller.js
@@ -95,7 +95,6 @@ const updateUser = (req, res) => {
     });
 };
 
-
 module.exports = {
     getUsers,
     getUserById,
